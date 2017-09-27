64dbab6b7673bfedb4eb090d4554ae8f842e58a2
miyaluc <srlucas3@gmail.com>
2017-09-27 12:18:32 -0700

    set up the controller and view folders

---
+++ lab12miya/lab12miya/Controllers/ErrorPageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab12miya.Controllers
+{
+    public class ErrorPageController
+    {
+    }
+}
